e14b7681447b07b46deee88c9151f80531861c12
221B328 <221B328@juet.co.in>
2025-01-15 15:25:30 +0530

    Lab 0

---
+++ LE-0/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Juet person = new Juet();
+
+        person.setName("Samriddhi");
+        person.setAge(20);
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
+}
